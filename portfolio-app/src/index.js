cef91800788fc93ba1beb9ad979cb30b11ed48c2
Charles Beach <charlesbeach@Charless-MacBook-Pro.local>
2019-09-09 13:08:01 -0400

    import BrowserRouter as Router from react-router-dom in index.js

--- portfolio-app/src/index.js
+++ portfolio-app/src/index.js
@@ -1,7 +1,8 @@
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-import App from './App';
 
 ReactDOM.render(
     <Router>
